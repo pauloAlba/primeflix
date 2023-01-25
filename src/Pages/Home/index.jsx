405f99f9c6606038f3bcf25c29a50072834db28b
Paulo Alba <pr2013com@hotmail.com>
2023-01-25 11:42:43 -0300

    Loading e página de erro adicionados

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
-import {Link} from "react-router-dom"
-import "./home.css"
+import { Link } from "react-router-dom";
+import "./home.css";
+
 
 // URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -20,32 +22,42 @@ function Home() {
 
       //console.log(response.data.results.slice(0, 10));
       setFilmes(response.data.results.slice(0, 10));
+      setLoading(false)
     }
 
     loadFilmes();
   }, []);
 
+  if(loading){
+    <div className="loading">
+      <h2>Carregando filmes...</h2>
+    </div>
+  }
+
   return (
     <div className="container">
       <div className="lista-filmes">
-        {filmes.map((filme)=> {
-          return(
+        {filmes.map((filme) => {
+          return (
             <article key={filme.id}>
-              <img width={100} src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+              <img
+                width={100}
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
               <br />
               <Link to={`/filme/${filme.id}`}>Acessar</Link>
               <br />
               <strong>{filme.title}</strong>
               <br />
               <strong>Data de lançamento: {filme.release_date}</strong>
-              <br />             
+              <br />
               <strong>{filme.overview}</strong>
               <hr />
             </article>
-          )
+          );
         })}
       </div>
-      
     </div>
   );
 }
