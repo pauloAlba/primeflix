ae48b80e9cc21a1de72ef19abc9cf4294c3076a3
Paulo Alba <pr2013com@hotmail.com>
2023-01-25 19:12:01 -0300

    Detalhes página filmes adicionados

--- src/Pages/Filme/index.jsx
+++ src/Pages/Filme/index.jsx
@@ -1,10 +1,56 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function Filme(){
-  return(
-    <div>
-      <h1>BEM VINDO A PAGINA DETALHES DO FILME</h1>
+import api from "../../Services/api";
+
+import "./filme-info.css"
+
+function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "28fc232cc001c31e8a031f419d0a14ca",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("FILME NÃO ENCONTRADO");
+        });
+    }
+    loadFilme();
+
+    return () => {
+      console.log("COMPONENTE FOI DESMONTADO");
+    };
+  }, []);
+ 
+  if(loading){
+    return(
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+    
+  }
+  return (
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
     </div>
-  )
+  );
 }
 
 export default Filme;
