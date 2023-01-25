6acbaa4bbad1766c1439f96852d07840df431da1
Paulo Alba <pr2013com@hotmail.com>
2023-01-25 19:53:11 -0300

    trailer do filme adicionado

--- src/Pages/Filme/index.jsx
+++ src/Pages/Filme/index.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import api from "../../Services/api";
 
-import "./filme-info.css"
+import "./filme-info.css";
 
 function Filme() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [filme, setFilme] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -25,6 +26,8 @@ function Filme() {
         })
         .catch(() => {
           console.log("FILME NÃO ENCONTRADO");
+          navigate("/", { replace: true });
+          return;
         });
     }
     loadFilme();
@@ -32,23 +35,34 @@ function Filme() {
     return () => {
       console.log("COMPONENTE FOI DESMONTADO");
     };
-  }, []);
- 
-  if(loading){
-    return(
+  }, [navigate, id]);
+
+  if (loading) {
+    return (
       <div className="filme-info">
         <h1>Carregando detalhes...</h1>
       </div>
-    )
-    
+    );
   }
   return (
     <div className="filme-info">
       <h1>{filme.title}</h1>
-      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
       <h3>Sinopse</h3>
       <span>{filme.overview}</span>
       <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} trailer` }>
+            Trailer
+          </a>
+        </button>
+      </div>
     </div>
   );
 }
